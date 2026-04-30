ac9522f7d04944edb799c347253f5f46baedb4db
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-04-30 11:52:42 +0530

    Demonstrate malloc, calloc and free in C

---
+++ DynamicMemoryMalloc.c
@@ -0,0 +1,41 @@
+ #include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+
+    int size = 0 ; 
+    float *marks = NULL;
+    int i = 0 ;                     // loop counter 
+
+    printf("Enter number of elements :\n");
+    scanf("%d" , &size);
+
+    // Dynamic memory allocation 
+
+    marks = (float * )malloc(size * sizeof(float));                         // here without type casting our program also run .
+                                                                            // we can also use malloc function in c++ programming to dynamic memory allocation but there typecasting is mandatory 
+    printf("Enter your marks : \n");
+
+    // Iteration 
+
+    //     1       2      3
+    for (i = 0 ; i < size ; i++)
+    {
+        scanf("%f", &marks[i]);     //4
+    }    
+   
+
+    printf("Entered marks are :\n");
+
+   //     1       2      3
+    for (i = 0 ; i < size ; i++)
+    {
+        printf("%f\n", marks[i]);     //4
+    }  
+
+    free(marks);                // free the memory 
+
+    return 0 ;
+}  
