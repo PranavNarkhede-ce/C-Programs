7283ba2937c5ae24341f8fb5266948c099f87a6b
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-03 17:58:06 +0530

    Create and implement user-defined header file

---
+++ mathematics_project/Helper.c
@@ -0,0 +1,11 @@
+// Function defination
+
+int Addition(int a , int b )
+{
+    return a + b ;
+}
+
+int Substraction(int a , int b )
+{
+    return a - b ;
+}
---
+++ mathematics_project/Marvellous.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+
+// function prototype or function declaration 
+int Addition ( int , int );
+
+int Substraction( int , int );
